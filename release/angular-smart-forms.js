3ad117b2da93663604aacea0a7494d274ec7ac69
André Werlang <beppe85@gmail.com>
2015-06-19 14:06:00 -0300

    chore(release): v0.1.1-beta.0

--- release/angular-smart-forms.js
+++ release/angular-smart-forms.js
@@ -75,12 +75,13 @@
             require: "ngModel",
             link: function(scope, element, attrs, ngModelCtrl) {
                 ngModelCtrl.$parsers.push(function isoDateParser(value) {
-                    console.debug("parse", typeof value, value);
-                    return value && value.toISOString().substring(0, 10) + "T00:00:00.000Z";
+                    if (angular.isDate(value)) {
+                        return value.toISOString().substring(0, 10) + "T00:00:00.000Z";
+                    }
+                    return value;
                 });
                 ngModelCtrl.$formatters.push(function isoDateFormatter(value) {
-                    console.debug("format", typeof value, value);
-                    if (typeof value === "string") {
+                    if (angular.isString(value)) {
                         var regex = /(\d{4})-(\d{2})-(\d{2})T?.*/;
                         var dateParts = regex.exec(value);
                         if (dateParts) {
@@ -92,6 +93,99 @@
             }
         };
     }
+    function numberInput() {
+        return {
+            restrict: "A",
+            require: "ngModel",
+            link: function(scope, element, attrs, ngModelCtrl) {
+                ngModelCtrl.$parsers.push(function(value) {
+                    if (value == null) return value;
+                    if (value.trim() === "") return null;
+                    return parseToModel(value);
+                });
+                ngModelCtrl.$formatters.push(function(value) {
+                    if (value == null) return value;
+                    return formatWithThousandSep(value);
+                });
+                element.on("focusout", function(e) {
+                    var inputVal = element.val();
+                    if (inputVal === "") return;
+                    element.val(formatWithDecimalSep(inputVal));
+                });
+                element.on("input", function(e) {
+                    var inputVal = element.val();
+                    var res = formatOnInput(inputVal);
+                    if (ngModelCtrl.$viewValue === res) return;
+                    scope.$apply(function() {
+                        ngModelCtrl.$setViewValue(res);
+                        ngModelCtrl.$render();
+                    });
+                });
+            }
+        };
+    }
+    function splice(intPart, idx, rem, s) {
+        return intPart.slice(0, idx) + s + intPart.slice(idx + Math.abs(rem));
+    }
+    function formatWithThousandSep(nStr) {
+        nStr += "";
+        var x = nStr.split(".");
+        var x1 = x[0];
+        var x2 = x.length > 1 ? "," + x[1] : "";
+        var rgx = /(\d+)(\d{3})/;
+        while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, "$1" + "." + "$2");
+        }
+        return x1 + x2;
+    }
+    function formatWithDecimalSep(value) {
+        var regex = /\,(\d*)/;
+        var result = regex.exec(value);
+        var decPart = 2 - (result ? result[1].length : 0);
+        if (result && result[0] == null) value = "0" + value;
+        if (result === null) value += ",";
+        while (decPart-- > 0) value += "0";
+        return value;
+    }
+    function parseToModel(value) {
+        return parseFloat(value.replace(/\./g, "").replace(",", "."));
+    }
+    function formatOnInput(value) {
+        var inputVal = value;
+        while (inputVal.charAt(0) === "0" && inputVal.charAt(0) !== ",") {
+            inputVal = inputVal.substr(1);
+        }
+        if (inputVal.charAt(0) === ",") inputVal = "0" + inputVal;
+        inputVal = inputVal.replace(/[^\d.\',']/g, "");
+        var point = inputVal.indexOf(",");
+        if (point >= 0) {
+            inputVal = inputVal.slice(0, point + 3);
+        }
+        var decimalSplit = inputVal.split(",");
+        var intPart = decimalSplit[0];
+        var decPart = decimalSplit[1];
+        intPart = intPart.replace(/[^\d]/g, "");
+        if (intPart.length > 3) {
+            var intDiv = Math.floor(intPart.length / 3);
+            while (intDiv > 0) {
+                var lastComma = intPart.indexOf(".");
+                if (lastComma < 0) {
+                    lastComma = intPart.length;
+                }
+                if (lastComma - 3 > 0) {
+                    intPart = splice(intPart, lastComma - 3, 0, ".");
+                }
+                intDiv--;
+            }
+        }
+        if (decPart === undefined) {
+            decPart = "";
+        } else {
+            decPart = "," + decPart;
+        }
+        var res = intPart + decPart;
+        return res;
+    }
     "use strict";
-    angular.module("wt.smart", []).directive("wtAutoFocus", [ autoFocus ]).directive("wtSmartForm", [ smartForm ]).directive("wtIsoDate", [ isoDate ]);
+    angular.module("wt.smart", []).directive("wtAutoFocus", [ autoFocus ]).directive("wtSmartForm", [ smartForm ]).directive("wtIsoDate", [ isoDate ]).directive("wtNumber", [ numberInput ]);
 })();
